3463af3bdeb0a2ee6fd26f328391ddbc94a8cde0
Gustavo Franco <guhenf@gmail.com>
2022-07-28 16:04:01 -0300

    Fix: ReposType url_html to html_url

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -16,7 +16,7 @@ interface ReposType {
   name: string;
   language: string;
   description: string;
-  git_html: string;
+  html_url: string;
   homepage: string;
 }
 
@@ -77,7 +77,7 @@ export const Project = (): JSX.Element => {
             {repository.description}
           </Text>
           <ProjectLinks>
-            <ProjectLink target="_blank" href={repository.git_html}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
             {repository.homepage && (
